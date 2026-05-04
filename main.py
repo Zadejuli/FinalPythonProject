1d4fc0eeaef05369de440647e22bbf7b3b287cb5
Branch A <z@p>
2026-05-04 12:46:13 -0400

    GUI is ready, starting on questions now.

--- main.py
+++ main.py
@@ -1,16 +1,92 @@
-# This is a sample Python script.
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+root = tk.Tk()
+root.title("Impossible Quiz")
+root.geometry("900x600")
+root.configure(bg="black")
+root.resizable(False, False)
+
+main_frame = tk.Frame(
+    root,
+    bg="#d9d9d9",
+    width=750,
+    height=500,
+    highlightbackground="black",
+    highlightthickness=4
+)
+
+main_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+question_label = tk.Label(
+    main_frame,
+    text="Question Goes Here",
+    font=("Arial", 22, "bold"),
+    bg="#d9d9d9",
+    fg="black",
+    wraplength=600
+)
+
+question_label.place(x=5, y=25, anchor="center")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+question_label.place(relx=0.5, rely=0.45, anchor="center")
 
+button_font = ("Arial", 18, "bold")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+red_button = tk.Button(
+    main_frame,
+    text="Answer 1",
+    bg="red",
+    fg="black",
+    font=button_font,
+    width=18,
+    height=5,
+    bd=5,
+    relief="raised"
+)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+red_button.place(x=70, y=40)
+
+blue_button = tk.Button(
+    main_frame,
+    text="Answer 2",
+    bg="#00a2ff",
+    fg="black",
+    font=button_font,
+    width=18,
+    height=5,
+    bd=5,
+    relief="raised"
+)
+
+blue_button.place(x=410, y=40)
+
+yellow_button = tk.Button(
+    main_frame,
+    text="Answer 3",
+    bg="yellow",
+    fg="black",
+    font=button_font,
+    width=18,
+    height=5,
+    bd=5,
+    relief="raised"
+)
+
+yellow_button.place(x=70, y=280)
+
+green_button = tk.Button(
+    main_frame,
+    text="Answer 4",
+    bg="green",
+    fg="black",
+    font=button_font,
+    width=18,
+    height=5,
+    bd=5,
+    relief="raised"
+)
+
+green_button.place(x=410, y=280)
+
+root.mainloop()
